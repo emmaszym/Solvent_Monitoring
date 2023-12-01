baaeb8a57372286ef7cd6a76f20c8dfc0bef9390
Team127 <emmaszym@udel.edu>
2023-12-01 15:49:48 -0500

    Change from f-strings to strings

--- OurRoboflow.py
+++ OurRoboflow.py
@@ -7,12 +7,12 @@ res = requests.post(
 	f"{BASE_URL}/{model_id}?"
 	+"&".join(
 		[
-			#f"api_key={pKCEf1sGVJg10T4haShH}",
-			f"api_key={rf_37btToay9PR1mjSRRieEDv91xV53}",
-			f"confidence={50}",
-			f"overlap={50}",
-			f"image={pic_1L_285.jpg}",
-			f"max_detections={4}",
+			#"api_key=pKCEf1sGVJg10T4haShH",
+			"api_key=rf_37btToay9PR1mjSRRieEDv91xV53",
+			"confidence=50",
+			"overlap=50",
+			"image=pic_1L_285.jpg",
+			"max_detections=4",
 		]
 	)
 )
