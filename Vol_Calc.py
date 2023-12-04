c6a9b155516c73bc58942da06ec5d11c7e3e8316
Team127 <emmaszym@udel.edu>
2023-12-04 18:19:16 -0500

    Add equation for 500 mL bottle

--- Vol_Calc.py
+++ Vol_Calc.py
@@ -2,7 +2,7 @@
 
 bottle_size = input("Size of Bottle?")
 height = float(input("What is the solvent height?"))
-
+"""
 if bottle_size== '500':
 	diameter=86
 elif bottle_size == '1000':
@@ -12,8 +12,12 @@ elif bottle_size == '2000':
 else: 
 	print('Invalid Bottle Size')
 	bottle_size = input("Size of bottle?")
+"""
+if bottle_size == '500':
+	volume = 1136.628 -2.9902029*height 
 if bottle_size  == '1000':
 	volume= 1490.8378-3.8065466*height
 #volume = (diameter/2)*height
 print ("The volume is: ", volume )
 
+ 
