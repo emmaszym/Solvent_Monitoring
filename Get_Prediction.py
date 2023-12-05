284146a18d90ce2c11ef27ea064d205c6d724d2a
emmaszym <emmaszym@udel.edu>
2023-12-05 17:11:51 -0500

    We got volumesgit config --global user.name emmaszym

--- Get_Prediction.py
+++ Get_Prediction.py
@@ -5,7 +5,7 @@ import json
 
  
 
-cmd = "base64 pic_1L_285.jpg | curl -d @- https://detect.roboflow.com/solvent-level/1?api_key=pKCEf1sGVJg10T4haShH"
+cmd = "base64 pic_1L_327.jpg | curl -d @- https://detect.roboflow.com/solvent-level/2?api_key=pKCEf1sGVJg10T4haShH"
 
  
 
@@ -36,7 +36,7 @@ print(f"Number of predictions: {predictions}")
 for pred in range(predictions):
 
     conf = jsonData["predictions"][pred]["confidence"]
-    height = jsonData["predictions"][pred]["height"]
+    height = jsonData["predictions"][pred]["y"]
     ccity = jsonData["predictions"][pred]["class_id"]
 
     print(f"    prediction {pred}: Confidence {conf} height {height} class_id {ccity}")
@@ -54,10 +54,16 @@ for entry in range(predictions):
 		continue
 for heights in range(predictions):
 	if jsonData["predictions"][heights]["class_id"] == 3:
-		solvent_height = jsonData["predictions"][heights]["height"]
-
+		solvent_height = jsonData["predictions"][heights]["y"]
+solvent_height=solvent_height*0.126
+vol=250
 if bottle_type == 1:
-	volume = 1490.8378-3.8065466*solvent_height
-print(volume) 
-#print(jsonData) 
+	vol = 1361- (2.61*solvent_height) - (0.00257*(solvent_height**2))
+if bottle_type == 5:
+	vol = 990 - (1.9*solvent_height) - (0.002*(solvent_height**2))
+if bottle_type == 2:
+	vol  = 2488 - (5.33*solvent_height) - (0.0025*(solvent_height**2))	
+#vol = 1490.8378-3.8065466*solvent_height
+print(vol) 
+print(jsonData) 
 
